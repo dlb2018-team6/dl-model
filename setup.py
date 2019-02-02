ef212da1015921ab31dcfb06084b6562f5b2230e
yukiyoshisato <40360870+yukiyoshisato@users.noreply.github.com>
2019-02-02 18:37:39 +0900

    create base directory

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The final competition of Deep Learning Basics 2018 at UT',
+    author='Yukiyoshi Sato',
+    license='',
+)
